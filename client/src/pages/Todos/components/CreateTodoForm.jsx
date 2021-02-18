b5e56635519aabf92306e4a8c858cbe9ca7185bc
Maxim Molochkov <klaseca@gmail.com>
2021-02-19 00:13:37 +0400

    Add todo create

--- client/src/pages/Todos/components/CreateTodoForm.jsx
+++ client/src/pages/Todos/components/CreateTodoForm.jsx
@@ -2,6 +2,10 @@ import { Flex, Button } from '@chakra-ui/react';
 import { Formik, Form } from 'formik';
 import FormikInput from 'components/FormikInput';
 import * as yup from 'yup';
+import { useEffect, useState } from 'react';
+import { useWs } from 'hooks/useWs';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectEmail, addTodo } from 'store/userSlice';
 
 const CreateTodoForm = ({ onClose }) => {
   const validationSchema = yup.object({
@@ -9,10 +13,38 @@ const CreateTodoForm = ({ onClose }) => {
     description: yup.string(),
   });
 
+  const ws = useWs({ url: 'ws://localhost:3001' });
+
+  const [error, setError] = useState('');
+
+  const email = useSelector(selectEmail);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (ws.data && ws.data.message.event === 'addTodo') {
+      const { error, data } = ws.data.message;
+      if (error) {
+        setError(error);
+      } else {
+        dispatch(addTodo(data));
+        onClose();
+      }
+    }
+  }, [ws.data, dispatch, onClose]);
+
   return (
     <Formik
       initialValues={{ name: '', description: '' }}
       validationSchema={validationSchema}
+      onSubmit={async (values) => {
+        console.log(values);
+        const dateCreated = new Intl.DateTimeFormat('ru-RU').format(new Date());
+        const request = {
+          event: 'addTodo',
+          payload: { ...values, dateCreated, email },
+        };
+        ws.send(request);
+      }}
     >
       <Form>
         <FormikInput name='name' label='Name' placeholder='Todo name' />
@@ -26,7 +58,7 @@ const CreateTodoForm = ({ onClose }) => {
         />
 
         <Flex justify='center' p='2'>
-          <Button colorScheme='telegram' mr={3} onClick={onClose}>
+          <Button type='submit' colorScheme='telegram' mr={3}>
             Save
           </Button>
           <Button colorScheme='red' onClick={onClose}>
