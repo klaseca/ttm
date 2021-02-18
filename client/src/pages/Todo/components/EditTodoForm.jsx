32b2f39f658624c3069a444d8ba330eddc4aa796
Maxim Molochkov <klaseca@gmail.com>
2021-02-19 00:12:55 +0400

    Add todo update

--- client/src/pages/Todo/components/EditTodoForm.jsx
+++ client/src/pages/Todo/components/EditTodoForm.jsx
@@ -3,20 +3,52 @@ import { Formik, Form } from 'formik';
 import FormikInput from 'components/FormikInput';
 import * as yup from 'yup';
 import { useHistory } from 'react-router-dom';
+import { useWs } from 'hooks/useWs';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectEmail, updateTodo } from 'store/userSlice';
+import { useEffect, useState, useCallback } from 'react';
 
 const EditTodoForm = ({ todo }) => {
   const history = useHistory();
-  const toHome = () => history.push('/');
+  const toHome = useCallback(() => history.push('/'), [history]);
 
   const validationSchema = yup.object({
     name: yup.string().required('Required'),
     description: yup.string(),
   });
 
+  const ws = useWs({ url: 'ws://localhost:3001' });
+
+  const [error, setError] = useState('');
+
+  const email = useSelector(selectEmail);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (ws.data && ws.data.message.event === 'updateTodo') {
+      const { error, data } = ws.data.message;
+      if (error) {
+        setError(error);
+      } else {
+        dispatch(updateTodo(data));
+        toHome();
+      }
+    }
+  }, [ws.data, dispatch, toHome]);
+
   return (
     <Formik
       initialValues={{ name: todo.name, description: todo.description }}
       validationSchema={validationSchema}
+      onSubmit={async (values) => {
+        console.log(values);
+        const request = {
+          event: 'updateTodo',
+          payload: { email, id: todo.id, ...values },
+        };
+        console.log(ws);
+        ws.send(request);
+      }}
     >
       <Flex
         as={Form}
