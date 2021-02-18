55c75629d83e7f5753892feb62ffe7d288a42f71
Maxim Molochkov <klaseca@gmail.com>
2021-02-19 00:16:43 +0400

    Fix type parametr

--- client/src/pages/Todo/Todo.jsx
+++ client/src/pages/Todo/Todo.jsx
@@ -6,7 +6,7 @@ import { Flex } from '@chakra-ui/react';
 
 const Todo = () => {
   const { id } = useParams();
-  const todo = useSelector((state) => selectTodoById(state, +id));
+  const todo = useSelector((state) => selectTodoById(state, id));
   return (
     <Flex h='100%' justify='center' align='center'>
       <EditTodoForm todo={todo} />
